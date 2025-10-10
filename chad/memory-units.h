3b40240f07efec57bc1d698815406423f38a3f60
anon <anon@anon.anon>
2025-10-10 15:28:34 +0200

    +memory-units

---
+++ chad/memory-units.h
@@ -0,0 +1,13 @@
+#ifndef MEMORY_UNITS
+#define MEMORY_UNITS
+
+#define KB  1000ull
+#define MB  1000000ull
+#define GB  1000000000ull
+#define TB  1000000000000ull
+#define KiB 1024ull
+#define MiB 1048576ull
+#define GiB 1073741824ull
+#define TiB 1099511627776ull
+
+#endif
