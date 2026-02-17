42541df62871aaf403c0d62de1fdb44c862f2ea5
Emil Williams <emilemilemil@cock.li>
2026-02-17 23:43:50 +0000

    terry

--- chad.h
+++ chad.h
@@ -5,9 +5,12 @@
 #include "chad/terminal.h"
 #include "chad/dictate.h"
 #include "chad/qx.h"
+#include "chad/terry.h"
 
 // external code
 #include "chad/sds.h"
 #include "chad/kvec.h"
 #include "chad/klist.h"
 #include "chad/remove_all.h"
+
+#endif
