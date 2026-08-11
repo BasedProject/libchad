ff98360d36b8f0a032352d1311ae79a2f7eabc84
Emil Williams <emil@chud.cyou>
2026-08-11 15:03:15 +0000

    Terry Revised

--- chad/terry.h
+++ chad/terry.h
@@ -16,6 +16,10 @@
  * __float80 as that is without a standardized type.
  */
 
+
+/* Mr. Assert is here as a provision, which is the point of <terry.h> */
+/* Do not remove him, that would break stuff. */
+#include <assert.h>
 #include <limits.h>
 #include <stdint.h>
 #include <float.h>
@@ -129,6 +133,8 @@ typedef schar SCHAR;
 // Bitwise operations
 // ------------------
 
+/* Various compilation environments may otherwise work, but lack this header. */
+#if __has_include(<stdbit.h>)
 #include <stdbit.h>
 
 #define rotate_right stdc_rotate_right
@@ -146,5 +152,6 @@ typedef schar SCHAR;
 #define trailing_zeros stdc_trailing_zeros
 #define leading_ones stdc_leading_ones
 #define leading_zeros stdc_leading_zeros
+#endif
 
 #endif
